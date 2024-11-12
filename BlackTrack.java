c1b47128a8f824e5d6d820ec00152b7abc26ef4e
andy33366 <andy704389@gmail.com>
2024-11-12 12:25:32 -0600

    first draft skeleton

---
+++ BlackTrack.java
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ *
+ *
+ */
+
+public class BlackTrack{
+
+	public static void main(String[] args){
+	
+		//
+	}
+	//api call to get grades
+	public static int getGrades(){
+	}
+	//calculate gpa based on grades
+	public static double calcGPA(grades){
+	}
+}
