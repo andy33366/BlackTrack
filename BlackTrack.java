5c2c847179b43619ab31f27a2f76a0b08ec9e36a
andy33366 <andy704389@gmail.com>
2024-11-12 15:56:16 -0600

    attempting to make an http request

--- BlackTrack.java
+++ BlackTrack.java
@@ -5,6 +5,9 @@
  *
  */
 import org.json.*;
+import java.net.URL;
+import java.net.HTTP;
+import java.net.HttpURLConnection;
 import java.util.Scanner;
 
 
@@ -15,10 +18,17 @@ public class BlackTrack{
 
 		String username = "";
 		String password = "";
+		String jSessionID = "";
+		final String COOKIE_CONSENT_ACCEPTED="true";
+		String AWSELB = "";
+		String AWSELBCORS = "";
+		String BbClientCalendarTimeZone="";
+		String BbRouter="";
+
 
 		Scanner input = new Scanner(System.in);
 	
-		System.out.println("Welcome to BlackTrack\nIn order to access the Blackboard API, you will need to give the program your login information.");
+		System.out.println("Welcome to BlackTrack\nIn order to access the Blackboard API, you will need to give the program your login information. If you are not properly logged in, the program will not be able to access your grades");
 		System.out.print("Username:");
 		username = input.nextLine();
 		System.out.print("Password:");
@@ -30,6 +40,11 @@ public class BlackTrack{
 		//make a get request to /ultra/grades
 		//grab AWSELB and AWSELBCORS
 		//
+		//HttpRequest init = Http.Request.newBuilder()
+		//	.uri(new URI("https://tamusa.blackboard.com/ultra/grades"))
+		//	.header("Cookie", "COOKIE_CONSENT_ACCEPTED="+COOKIE_CONSENT_ACCEPTED+"")
+		//	.GET()
+		//	.build();
 		//make get request to /?new_loc=%2Fultra%2Fgrades
 		//send AWSELB, AWSELBCORS, and COOKIE_CONSENT_ACCEPTED=true
 		//grab JSESSIONID and BbRouter
@@ -41,14 +56,33 @@ public class BlackTrack{
 		//
 	}
 	//api call to get classes
-	public static String[] getClasses(){
+	public static String[] getCourses(){
 		//get /learn/api/v1/users/<userId>/memberships?expand=course.effectievAvailability&includeCount=true&limit=10000
+		//grab first courseId field
+		//append courseId to courses[]
 	}
 	//api call to get grades
-	public static int[] getGrades(classes){
+	public static double[] getGrades(courses[]){
+		//for i in courses[]
+		//get /learn/api/v1/courses/<courseId>/gradebook/grades?limit=100&userId=<userId>
+		//grab manualGrade and pointsPossible
+		//courseGrade = manualgrade/pointspossible
+		//append courseGrade to grades[]
 	}
 	//calculate gpa based on grades
 	public static double calcGPA(grades){
+		//for grade in grades[] 
+		//case grade < 0.65 --> 0.0
+		//case grade < 0.67 --> 1.0
+		//case grade < 0.7 --> 1.3
+		//case grade < 0.73 --> 1.7
+		//case grade < 0.77 __> 2.0
+		//case grade < 0.8 --> 2.3
+		//case grade < 0.83 --> 2.7
+		//case grade < 0.87 --> 3.0
+		//case grade < 90 --> 3.3
+		//case grade < 93 --> 4.0
+		//default 4.0
 	}
 	//print message based on gpa
 	public static void printMessage(gpa){
